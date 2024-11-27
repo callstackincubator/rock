2e662562571c267e99a11c93b2a86719517a646f
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-11-27 18:36:52 +0100

    fix test

--- packages/create-app/src/lib/__tests__/bin.test.ts
+++ packages/create-app/src/lib/__tests__/bin.test.ts
@@ -3,18 +3,18 @@ import { PLATFORMS } from '../templates.js';
 
 test('formatConfig', () => {
   expect(formatConfig(PLATFORMS)).toMatchInlineSnapshot(`
-        "import { pluginPlatformIOS } from '@callstack/rnef-plugin-platform-ios';
-        import { pluginPlatformAndroid } from '@callstack/rnef-plugin-platform-android';
-    
-        export default {
-          plugins: {},
-          platforms: {
-            ios: pluginPlatformIOS(),
-            android: pluginPlatformAndroid(),
-          },
-        };
-        "
-      `);
+    "import { pluginPlatformIOS } from '@callstack/rnef-plugin-platform-ios';
+    import { pluginPlatformAndroid } from '@callstack/rnef-plugin-platform-android';
+
+    export default {
+      plugins: {},
+      platforms: {
+        ios: pluginPlatformIOS(),
+        android: pluginPlatformAndroid(),
+      },
+    };
+    "
+  `);
 
   expect(formatConfig([PLATFORMS[0]])).toMatchInlineSnapshot(`
     "import { pluginPlatformIOS } from '@callstack/rnef-plugin-platform-ios';
