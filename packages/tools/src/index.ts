b15a45387e7f10b03cea5da40d056d3b2b3e92dc
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-12-12 16:43:31 +0100

    detect project root and ci

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -3,3 +3,5 @@ export { default as logger } from './lib/logger.js';
 export { nativeFingerprint } from './lib/fingerprint.js';
 export { default as cacheManager } from './lib/cacheManager.js';
 export * from './lib/path.js';
+export * from './lib/ci.js';
+export * from './lib/project.js';
