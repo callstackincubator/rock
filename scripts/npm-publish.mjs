41fd9f0675a8a46f7ee9bfbfb13e798e0ed4fbfd
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-12-18 15:45:45 +0100

    improve

--- scripts/npm-publish.mjs
+++ scripts/npm-publish.mjs
@@ -1,60 +1,52 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { intro, log, spinner, outro } from '@clack/prompts';
+import { intro, spinner, log, outro } from '@clack/prompts';
+import minimist from 'minimist';
 import spawn from 'nano-spawn';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const ROOT_DIR = path.join(__dirname, '..');
 
-const VERDACCIO_REGISTRY_URL = `http://localhost:4873`;
-const VERDACCIO_STORAGE_PATH = '/tmp/verdaccio-storage';
-
 const loader = spinner();
+const args = minimist(process.argv.slice(2));
+const isVerbose = args.verbose || args.v;
 
 async function run() {
-  intro('Verdaccio: publishing all packages');
+  intro('NPM: privately publishing all packages');
 
   try {
-    await clearPackageStorage();
     await publishPackages();
     await publishTemplate();
     outro('Done');
   } catch (error) {
-    clearPackageStorage();
     console.error('Error', error);
     process.exit(1);
   }
 }
 
-async function clearPackageStorage() {
-  loader.start('Clearing package storage');
-  await spawn('rm', ['-rf', VERDACCIO_STORAGE_PATH]);
-  loader.stop('Cleared package storage');
-}
-
 async function publishPackages() {
-  loader.start('Publishing all packages to Verdaccio');
+  loader.start('Publishing all packages to NPM (private)');
 
   // This is a workaround to make pnpm publish work with our templates.
   // PNPM removes execute (+x) flag from files in package, e.g. gradlew, so
   // we use `npm publish` instead of `pnpm publish` to publish packages.
   // This also prevents us from using `workspace:` dependencies.
   // This is a known issue: https://github.com/pnpm/pnpm/issues/8862
-  await spawn('pnpm', ['nx', 'run-many', '-t', 'publish:verdaccio']);
-  loader.stop('Published all packages.');
+  const output = await spawn('pnpm', ['nx', 'run-many', '-t', 'publish:npm']);
+  if (isVerbose) {
+    log.message(output.stdout.toString());
+  }
+  loader.stop('Published all packages to NPM (private).');
 }
 
 async function publishTemplate() {
-  loader.start('Publishing template to Verdaccio');
-  await spawn(
-    'npm',
-    [
-      'publish',
-      `--registry=${VERDACCIO_REGISTRY_URL}`,
-      `--userconfig=${ROOT_DIR}/.npmrc`,
-    ],
-    { cwd: `${ROOT_DIR}/templates/rnef-template-default` }
-  );
+  loader.start('Publishing template to NPM (private)');
+  const output = await spawn('npm', ['publish', '--access', 'restricted'], {
+    cwd: `${ROOT_DIR}/templates/rnef-template-default`,
+  });
+  if (isVerbose) {
+    log.message(output.stdout.toString());
+  }
   loader.stop('Published template.');
 }
 
