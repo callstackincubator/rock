36ab40f166b7d752a9ede86b3e0f1bb83575fea6
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-12-31 12:26:43 +0100

    tweaks

--- packages/tools/src/lib/logger.ts
+++ packages/tools/src/lib/logger.ts
@@ -12,40 +12,34 @@ const SYMBOL_DEBUG = unicodeWithFallback('●', '•');
 
 let verbose = false;
 
-const formatMessages = (elements: Array<unknown>) =>
-  elements.map((e) => util.inspect(e)).join(' ');
-
-const mapLines = (text: string, colorFn: (line: string) => string) =>
-  text.split('\n').map(colorFn).join('\n');
-
-const success = (...messages: Array<string>) => {
-  const output = formatMessages(messages);
+const success = (...messages: Array<unknown>) => {
+  const output = util.format(...messages);
   clackLog.success(output);
 };
 
-const info = (...messages: Array<string>) => {
-  const output = formatMessages(messages);
+const info = (...messages: Array<unknown>) => {
+  const output = util.format(...messages);
   clackLog.info(output);
 };
 
-const warn = (...messages: Array<string>) => {
-  const output = formatMessages(messages);
+const warn = (...messages: Array<unknown>) => {
+  const output = util.format(...messages);
   clackLog.warn(mapLines(output, color.yellow));
 };
 
-const error = (...messages: Array<string>) => {
-  const output = formatMessages(messages);
+const error = (...messages: Array<unknown>) => {
+  const output = util.format(...messages);
   clackLog.error(mapLines(output, color.red));
 };
 
-const log = (...messages: Array<string>) => {
-  const output = formatMessages(messages);
+const log = (...messages: Array<unknown>) => {
+  const output = util.format(...messages);
   clackLog.step(output);
 };
 
 const debug = (...messages: Array<unknown>) => {
   if (verbose) {
-    const output = formatMessages(messages);
+    const output = util.format(...messages);
     clackLog.message(mapLines(output, color.dim), {
       symbol: color.dim(SYMBOL_DEBUG),
     });
@@ -68,3 +62,7 @@ export default {
   setVerbose,
   isVerbose,
 };
+
+function mapLines(text: string, colorFn: (line: string) => string) {
+  return text.split('\n').map(colorFn).join('\n');
+}
