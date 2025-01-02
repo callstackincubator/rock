8a68f517d7a5425e57201d2ce4f2b431aebaf808
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-01-02 17:23:32 +0100

    .

--- packages/tools/src/lib/cacheManager.ts
+++ packages/tools/src/lib/cacheManager.ts
@@ -5,68 +5,67 @@ import { RnefError } from './error.js';
 import logger from './logger.js';
 import { getCacheRootPath } from './project.js';
 
+const CACHE_FILE_NAME = 'project.json';
+
 type CacheKey = string;
 type Cache = { [key in CacheKey]?: string };
 
-function loadCache(name: string): Cache | undefined {
+function getCacheFile() {
+  return path.join(getCacheRootPath(), CACHE_FILE_NAME);
+}
+
+function loadCache(): Cache {
   try {
-    const cacheRaw = fs.readFileSync(
-      path.resolve(getCacheRootPath(), name),
-      'utf8'
-    );
-    return JSON.parse(cacheRaw);
-  } catch (e) {
-    if ((e as { code: string }).code === 'ENOENT') {
-      // Create cache file since it doesn't exist.
-      saveCache(name, {});
+    const cachePath = path.resolve(getCacheFile());
+    if (!fs.existsSync(cachePath)) {
+      logger.debug('No cache found');
+      return {};
     }
-    logger.debug('No cache found');
-    return undefined;
+
+    const content = fs.readFileSync(cachePath, 'utf8');
+    return JSON.parse(content);
+  } catch (error) {
+    logger.warn('Failed to load cache', error);
+    return {};
   }
 }
 
-function saveCache(name: string, cache: Cache) {
-  const cachePath = path.resolve(getCacheRootPath(), name);
+function saveCache(cache: Cache) {
+  const cachePath = path.resolve(getCacheFile());
   fs.mkdirSync(path.dirname(cachePath), { recursive: true });
   fs.writeFileSync(cachePath, JSON.stringify(cache, null, 2));
 }
 
-function removeProjectCache(name: string) {
-  const cacheRootPath = getCacheRootPath();
+function removeProjectCache() {
   try {
-    const cachePath = path.resolve(cacheRootPath, name);
+    const cachePath = path.resolve(getCacheFile());
     if (fs.existsSync(cachePath)) {
       fs.rmSync(cachePath, { recursive: true });
     }
   } catch (error) {
     throw new RnefError(
       `Failed to remove cache for ${name}. If you experience any issues when running freshly initialized project, please remove the "${color.underline(
-        path.join(cacheRootPath, name)
+        path.join(getCacheFile())
       )}" folder manually.`,
       { cause: error }
     );
   }
 }
 
-function get(name: string, key: CacheKey): string | undefined {
-  const cache = loadCache(name);
-  if (cache) {
-    return cache[key];
-  }
-  return undefined;
+function get(key: CacheKey): string | undefined {
+  const cache = loadCache();
+  return cache[key];
 }
 
-function set(name: string, key: CacheKey, value: string) {
-  const cache = loadCache(name);
-  if (cache) {
-    cache[key] = value;
-    saveCache(name, cache);
-  }
+function set(key: CacheKey, value: string) {
+  const cache = loadCache();
+  cache[key] = value;
+  saveCache(cache);
 }
 
 export default {
   get,
   set,
   removeProjectCache,
-  getCacheRootPath: getCacheRootPath,
+  getCacheRootPath,
 };
