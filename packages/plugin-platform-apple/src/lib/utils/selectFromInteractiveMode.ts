29de872c68ffaacb43ff1a7e451322d77d1d704c
szymonrybczak <szymon.rybczak@gmail.com>
2025-01-20 12:39:12 +0100

    fix: return preselected value

--- packages/plugin-platform-apple/src/lib/utils/selectFromInteractiveMode.ts
+++ packages/plugin-platform-apple/src/lib/utils/selectFromInteractiveMode.ts
@@ -24,19 +24,22 @@ export async function selectFromInteractiveMode(
     };
   }
 
-  let newScheme = preselectedScheme;
-  let newMode = preselectedMode;
+  let newScheme;
+  let newMode;
   const info = await getInfo(xcodeProject, sourceDir);
 
   const schemes = info?.schemes;
 
-  if (!preselectedScheme && schemes && schemes.length > 1) {
-    newScheme = await promptForSchemeSelection(schemes);
+  if (schemes && schemes.length > 1) {
+    newScheme = await promptForSchemeSelection(schemes, preselectedScheme);
   }
 
   const configurations = info?.configurations;
-  if (!preselectedMode && configurations && configurations.length > 1) {
-    newMode = await promptForConfigurationSelection(configurations);
+  if (configurations && configurations.length > 1) {
+    newMode = await promptForConfigurationSelection(
+      configurations,
+      preselectedMode
+    );
   }
 
   return {
