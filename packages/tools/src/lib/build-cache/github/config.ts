e44ca4e5c7b9ce0dec228da2f18b02e084cbf0f6
Michał Pierzchała <thymikee@gmail.com>
2025-01-21 22:18:10 +0100

    feat: prompt for remote when more than 1 set in forked projects

--- packages/tools/src/lib/build-cache/github/config.ts
+++ packages/tools/src/lib/build-cache/github/config.ts
@@ -1,5 +1,8 @@
+import { select } from '@clack/prompts';
 import spawn from 'nano-spawn';
+import cacheManager from '../../cacheManager.js';
 import logger from '../../logger.js';
+import { checkCancelPrompt } from '../../prompts.js';
 import { GITHUB_REPO_REGEX } from './patterns.js';
 
 export function hasGitHubToken(): boolean {
@@ -14,10 +17,30 @@ export type GitHubRepoDetails = {
 
 export async function detectGitHubRepoDetails(): Promise<GitHubRepoDetails | null> {
   try {
+    let gitRemote = cacheManager.get('gitRemote');
+    if (!gitRemote) {
+      const { output: remoteOutput } = await spawn('git', ['remote']);
+      const remotes = remoteOutput.split('\n').filter(Boolean);
+      if (remotes.length > 1) {
+        logger.log(); // create visual space
+        gitRemote = checkCancelPrompt<string>(
+          await select({
+            message: 'Select git remote of your original project:',
+            options: remotes.map((remote) => ({
+              value: remote,
+              label: remote,
+            })),
+          })
+        );
+        cacheManager.set('gitRemote', gitRemote);
+      } else {
+        gitRemote = remotes[0];
+      }
+    }
     const { output: url } = await spawn('git', [
       'config',
       '--get',
-      'remote.origin.url',
+      `remote.${gitRemote}.url`,
     ]);
 
     const match = url.match(GITHUB_REPO_REGEX);
