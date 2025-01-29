37a1d83d068a668cb0a196366c8fd5e1e7459e07
Michał Pierzchała <thymikee@gmail.com>
2025-01-29 20:16:49 +0100

    refactor: separate build and archive error flow

--- packages/plugin-platform-apple/src/lib/commands/build/createBuild.ts
+++ packages/plugin-platform-apple/src/lib/commands/build/createBuild.ts
@@ -54,15 +54,18 @@ export const createBuild = async (
       mode,
       args
     );
+  } catch (error) {
+    throw new RnefError('Failed to create build', { cause: error });
+  }
 
-    if (args.archive) {
-      const { archiveDir } = getBuildPaths(platformName);
+  if (args.archive) {
+    const { archiveDir } = getBuildPaths(platformName);
 
-      const archivePath = path.join(
-        archiveDir,
-        `${xcodeProject.name.replace('.xcworkspace', '')}.xcarchive`
-      );
-
+    const archivePath = path.join(
+      archiveDir,
+      `${xcodeProject.name.replace('.xcworkspace', '')}.xcarchive`
+    );
+    try {
       await exportArchive({
         sourceDir,
         archivePath,
@@ -71,10 +74,11 @@ export const createBuild = async (
         platformName,
         exportExtraParams: args.exportExtraParams ?? [],
       });
+
+      outro('Success 🎉.');
+    } catch (error) {
+      throw new RnefError('Failed to create archive', { cause: error });
     }
-    outro('Success 🎉.');
-  } catch (error) {
-    throw new RnefError('Failed to create build', { cause: error });
   }
 };
 
