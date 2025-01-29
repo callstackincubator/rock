25620025294c0fc35d148afb62e21ccbbf501138
Michał Pierzchała <thymikee@gmail.com>
2025-01-29 21:50:02 +0100

    cleanup

--- packages/plugin-platform-apple/src/lib/utils/prompts.ts
+++ packages/plugin-platform-apple/src/lib/utils/prompts.ts
@@ -3,28 +3,24 @@ import color from 'picocolors';
 import { sortByRecentDevices } from '../commands/run/recentDevices.js';
 import type { ApplePlatform, Device } from '../types/index.js';
 
-export function promptForSchemeSelection(schemes: string[], preselectedScheme?: string) {
-  return preselectedScheme
-    ? Promise.resolve(preselectedScheme)
-    : promptSelect({
-        message: 'Select the scheme you want to use',
-        options: schemes.map((value) => ({
-          label: value,
-          value: value,
-        })),
-      });
+export function promptForSchemeSelection(schemes: string[]) {
+  return promptSelect({
+    message: 'Select the scheme you want to use',
+    options: schemes.map((value) => ({
+      label: value,
+      value: value,
+    })),
+  });
 }
 
-export function promptForConfigurationSelection(configurations: string[], preselectedConfiguration?: string) {
-  return preselectedConfiguration
-    ? Promise.resolve(preselectedConfiguration)
-    : promptSelect({
-        message: 'Select the configuration you want to use',
-        options: configurations.map((value) => ({
-          label: value,
-          value: value,
-        })),
-      });
+export function promptForConfigurationSelection(configurations: string[]) {
+  return promptSelect({
+    message: 'Select the configuration you want to use',
+    options: configurations.map((value) => ({
+      label: value,
+      value: value,
+    })),
+  });
 }
 
 export function promptForDeviceSelection(
