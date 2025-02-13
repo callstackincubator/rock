cb6d1b6e577cbfe624b0a0b9419bf283a9fad92d
Michał Pierzchała <thymikee@gmail.com>
2025-02-13 20:42:27 +0100

    Update packages/plugin-platform-android/src/lib/paths.ts

--- packages/plugin-platform-android/src/lib/paths.ts
+++ packages/plugin-platform-android/src/lib/paths.ts
@@ -5,7 +5,7 @@ export function getAndroidSdkPath() {
   const sdkRoot =
     process.env['ANDROID_HOME'] || process.env['ANDROID_SDK_ROOT'];
   if (!sdkRoot) {
-    throw new Error('ANDROID_HOME or ANDROID_SDK_ROOT is not set');
+    throw new Error('ANDROID_HOME or ANDROID_SDK_ROOT environment variable is not set. Please follow instructions at: https://reactnative.dev/docs/set-up-your-environment?platform=android');
   }
   return sdkRoot;
 }
