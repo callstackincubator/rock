56ac48b15f63f178c9ba51d6a28e4a6de88cfcd1
Michał Pierzchała <thymikee@gmail.com>
2025-03-12 18:32:39 +0100

    docs: init

---
+++ website/rspress.config.ts
@@ -0,0 +1,23 @@
+import * as path from 'node:path';
+import { pluginCallstackTheme } from '@callstack/rspress-theme/plugin';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  plugins: [pluginCallstackTheme()],
+  root: path.join(__dirname, 'docs'),
+  title: 'React Native Enterprise Framework',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/callstack/rnef',
+      },
+    ],
+  },
+});
