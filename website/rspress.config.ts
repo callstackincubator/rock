92b652ae2e0e4062b07fa3810f7d871499d6c3e3
szymonrybczak <szymon.rybczak@gmail.com>
2025-03-17 14:20:52 +0100

    feat: add analytics

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -1,9 +1,10 @@
 import * as path from 'node:path';
 import { pluginCallstackTheme } from '@callstack/rspress-theme/plugin';
 import { defineConfig } from 'rspress/config';
+import vercelPluginAnalytics from 'rspress-plugin-vercel-analytics';
 
 export default defineConfig({
-  plugins: [pluginCallstackTheme()],
+  plugins: [pluginCallstackTheme(), vercelPluginAnalytics()],
   root: path.join(__dirname, 'docs'),
   title: 'React Native Enterprise Framework',
   icon: '/rspress-icon.png',
