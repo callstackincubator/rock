27692db0048144d67fdc8c8dd187990df17e9443
szymonrybczak <szymon.rybczak@gmail.com>
2025-04-24 11:15:48 +0200

    fix(apple): Improve device matching by name

--- packages/platform-apple-helpers/src/lib/commands/run/matchingDevice.ts
+++ packages/platform-apple-helpers/src/lib/commands/run/matchingDevice.ts
@@ -3,14 +3,17 @@ import type { Device } from '../../types/index.js';
 export function matchingDevice(devices: Array<Device>, deviceArg: string) {
   const deviceByName = devices.find(
     (device) =>
-      device.name === deviceArg || formattedDeviceName(device) === deviceArg
+      device.name === deviceArg ||
+      formattedDeviceName(device, true) === deviceArg ||
+      formattedDeviceName(device, false) === deviceArg
   );
   const deviceByUdid = devices.find((d) => d.udid === deviceArg);
   return deviceByName || deviceByUdid;
 }
 
-export function formattedDeviceName(simulator: Device) {
+export function formattedDeviceName(simulator: Device, replaceIOS: boolean) {
+  const bareVersion = simulator.version?.replace(/^iOS /, '');
   return simulator.version
-    ? `${simulator.name} (${simulator.version})`
+    ? `${simulator.name} (${replaceIOS ? bareVersion : simulator.version})`
     : simulator.name;
 }
