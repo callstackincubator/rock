3bc8a46144dc375605941d3dca14e80182030648
Adam Horodyski <ad.horodyski@gmail.com>
2025-05-09 11:30:21 +0200

    chore: move with-environment to the newly extracted script

--- packages/platform-ios/template/ios/react-native-xcode.sh
+++ packages/platform-ios/template/ios/react-native-xcode.sh
@@ -1,5 +1,7 @@
 set -e
 
+. "$REACT_NATIVE_PATH/scripts/xcode/with-environment.sh"
+
 export CONFIG_CMD="dummy-workaround-value"
 export CLI_PATH="$("$NODE_BINARY" --print "require('path').dirname(require.resolve('@rnef/cli/package.json')) + '/dist/src/bin.js'")"
 
