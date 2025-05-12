963e2a822dfae33f92bb0288b64f87d31318c5a4
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-05-12 17:33:31 +0200

    improve dx

--- packages/create-app/src/lib/utils/edit-template.ts
+++ packages/create-app/src/lib/utils/edit-template.ts
@@ -1,7 +1,6 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { renameFile, walkDirectory } from './fs.js';
-import { transformProjectNameToPascalCase } from './name.js';
 
 /**
  * Placeholder name used in template, that should be replaced with cleaned-up project name.
@@ -25,36 +24,37 @@ export function renameCommonFiles(projectPath: string) {
  * - Rename paths containing placeholder
  * - Replace placeholder in text files
  */
-export function replacePlaceholder(projectPath: string, projectName: string) {
-  const targetName = transformProjectNameToPascalCase(projectName);
-  if (targetName === PLACEHOLDER_NAME) {
-    return;
-  }
-
+export function replacePlaceholder(
+  projectPath: string,
+  normalizedName: string
+) {
   for (const filePath of walkDirectory(projectPath).reverse()) {
     if (!fs.statSync(filePath).isDirectory()) {
-      replacePlaceholderInTextFile(filePath, targetName);
+      replacePlaceholderInTextFile(filePath, normalizedName);
     }
 
     if (path.basename(filePath).includes(PLACEHOLDER_NAME)) {
-      renameFile(filePath, PLACEHOLDER_NAME, targetName);
+      renameFile(filePath, PLACEHOLDER_NAME, normalizedName);
     } else if (
       path.basename(filePath).includes(PLACEHOLDER_NAME.toLowerCase())
     ) {
       renameFile(
         filePath,
         PLACEHOLDER_NAME.toLowerCase(),
-        targetName.toLowerCase()
+        normalizedName.toLowerCase()
       );
     }
   }
 }
 
-function replacePlaceholderInTextFile(filePath: string, targetName: string) {
+function replacePlaceholderInTextFile(
+  filePath: string,
+  normalizedName: string
+) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
   const replacedFileContent = fileContent
-    .replaceAll(PLACEHOLDER_NAME, targetName)
-    .replaceAll(PLACEHOLDER_NAME.toLowerCase(), targetName.toLowerCase());
+    .replaceAll(PLACEHOLDER_NAME, normalizedName)
+    .replaceAll(PLACEHOLDER_NAME.toLowerCase(), normalizedName.toLowerCase());
 
   if (fileContent !== replacedFileContent) {
     fs.writeFileSync(filePath, replacedFileContent, 'utf8');
