75523fdf897844fa81ca78e8ed55a4121fffccab
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-01 01:14:50 +0200

    refactor: cleanup template

--- templates/rnef-template-default/App.tsx
+++ templates/rnef-template-default/App.tsx
@@ -2,12 +2,10 @@ import HomeIOS from '@rnef/platform-ios/welcome';
 import HomeAndroid from '@rnef/platform-android/welcome';
 import {Platform} from 'react-native';
 
-export function App() {
+export default function App() {
   const Home = Platform.select({
     ios: HomeIOS,
     android: HomeAndroid,
   });
   return <Home />;
 }
-
-export default App;
