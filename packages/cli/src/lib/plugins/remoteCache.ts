113264773c3b35367d65becc18a1de705d4d396a
Michał Pierzchała <thymikee@gmail.com>
2025-07-24 16:53:48 +0200

    update title

--- packages/cli/src/lib/plugins/remoteCache.ts
+++ packages/cli/src/lib/plugins/remoteCache.ts
@@ -307,7 +307,7 @@ function templateIndexHtml({
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Download iOS App</title>
+    <title>Download ${appName} for iOS</title>
     <style>
       * {
         margin: 0;
