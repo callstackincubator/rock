0430c28fb7e38467575990b932cea7f55b95294b
Michał Pierzchała <thymikee@gmail.com>
2025-08-13 20:01:32 +0200

    add types

--- packages/plugin-metro/src/lib/getReactNativeDeps.ts
+++ packages/plugin-metro/src/lib/getReactNativeDeps.ts
@@ -1,6 +1,9 @@
 import { createRequire } from 'node:module';
+import type devMiddleware from '@react-native/dev-middleware';
 
-export async function getDevMiddleware(reactNativePath: string) {
+export async function getDevMiddleware(
+  reactNativePath: string,
+): Promise<typeof devMiddleware> {
   const require = createRequire(import.meta.url);
   const reactNativeCommunityCliPluginPath = require.resolve(
     '@react-native/community-cli-plugin',
