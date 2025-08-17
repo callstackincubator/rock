c9e44f586752f7197e9e43101997963695f08b29
Thiago Brezinski <thiagobrez@gmail.com>
2025-08-17 19:30:37 +0100

    feat: add expo-config-plugins-test-app and remove other apps

--- apps/AppConfigPlugins/app.plugin.js
+++
@@ -1,8 +0,0 @@
-const { withInfoPlist } = require('@expo/config-plugins');
-
-module.exports = (config, id) => {
-  return withInfoPlist(config, config => {
-    console.log('>>>', config);
-    return config;
-  });
-};
--- apps/test-app/App.tsx
+++
@@ -1,5 +0,0 @@
-import WelcomeScreen from '@rnef/welcome-screen';
-
-export default function App() {
-  return <WelcomeScreen />;
-}
