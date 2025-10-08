e6809816c2d59f69b8eda389610ec3db6048e9da
Michał Pierzchała <thymikee@gmail.com>
2025-10-08 13:22:08 -0700

    fix linter

--- packages/platform-harmony/template/react-native.config.js
+++ packages/platform-harmony/template/react-native.config.js
@@ -1,7 +1,10 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   commands: [
+    // eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
     require('@react-native-oh/react-native-harmony-cli/dist/commands/link-harmony.js')
       .commandLinkHarmony,
+    // eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
     require('@react-native-oh/react-native-harmony-cli/dist/commands/codegen-harmony.js')
       .commandCodegenHarmony,
   ],
