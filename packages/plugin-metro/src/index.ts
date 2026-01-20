0cb3a7f2d837ae860ba68d1df8f38acb65054a99
Michał Pierzchała <thymikee@gmail.com>
2026-01-20 20:32:45 +0100

    add types

--- packages/plugin-metro/src/index.ts
+++ packages/plugin-metro/src/index.ts
@@ -1,3 +1,8 @@
 export { pluginMetro } from './lib/pluginMetro.js';
 export { startDevServer } from './lib/start/command.js';
-export { getDefaultConfig, mergeConfig } from './config.js';
+export {
+  getDefaultConfig,
+  mergeConfig,
+  type MetroConfig,
+  type InputConfig,
+} from './config.js';
